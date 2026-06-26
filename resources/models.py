5db277a851d7f3e0daa3215e5a7b3a11e6aef103
Pawel <pawel.hiszpanski@gmail.com>
2026-06-26 16:44:15 +0200

    first html file added

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=150)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.abbreviation})"
+
+
+class FieldOfStudy(models.Model):
+    name = models.CharField(max_length=100)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name="field_of_study")
+
+    def __str__(self):
+        return f"{self.name} - {self.faculty.name}"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=30, unique=True) # eg. ETI-INF-2026
+    semester = models.IntegerField()
+    field_of_study = models.ForeignKey(FieldOfStudy, on_delete=models.CASCADE, related_name="subjects")
+
+    def __str__(self):
+        return f"{self.name} (Semester: {self.semester} - {self.field_of_study.name})"
+
--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import FieldOfStudy
 
 # Create your views here.
+
+def index(request):
+    fields = FieldOfStudy.objects.all()
+
+    context = {
+        'fields': fields,
+    }
+
+    return render(request, 'resources/index.html', context)
